26c4af85775c797fe8018effa77d7138098eb30a
git <kontakt@bartoszzawadka.pl>
2017-02-05 22:24:59 +0100

    Added first part of controls in filtering/sorting pane

--- public/javascripts/liquids-controller.js
+++ public/javascripts/liquids-controller.js
@@ -3,6 +3,26 @@
  */
 angular.module('Moonshiner').controller('LiquidsController', function ($scope, $http, $routeParams, $location){
     $scope.liquids = [];
+    $scope.filter = {
+        isVisible: false,
+        icon: "expand_more",
+        filterIn: [
+            {name: "name", title: "Name", checked: true},
+            {name: "description", title: "Description", checked: true},
+            {name: "author", title: "Author", checked: true},
+            {name: "aromas", title: "Aromas", checked: true},
+            {name: "accessories", title: "Accessories", checked: true}
+        ]
+    };
+
+    $scope.toggleFilter = function(){
+        $scope.filter.isVisible = !$scope.filter.isVisible;
+        if($scope.filter.isVisible)
+            $scope.filter.icon = "expand_less";
+        else{
+            $scope.filter.icon = "expand_more";
+        }
+    };
 
     $http.get('/api/liquids').then(function(res){
         if(res.status && res.status != 200){
